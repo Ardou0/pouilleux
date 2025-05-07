bbb88fe300be68f887454185d780281d91d1ef4b
Ardou0 <armand.walle@gmail.com>
2025-05-07 12:53:00 +0200

    HotFix Main.java
    - Linked MainFrame to Main

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        MainFrame frame = new MainFrame();
-        frame.setVisible(true);
-
+        MainFrame.main(args);
     }
 }
