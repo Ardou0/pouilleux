4cd5e4a117f8ffcf29819acea10f690e83c9ed67
Ardou0 <armand.walle@gmail.com>
2025-05-07 13:15:31 +0200

    Update Main.java
    - Deleted all boring and useless stuff

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,4 @@
-import core.model.*;
-import core.persistence.*;
-import core.gui.*;
-
-import javax.swing.*;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import core.gui.MainFrame;
 
 public class Main {
     public static void main(String[] args) {
